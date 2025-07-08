4fa83aa9c566cb06ebdcaa9a228e28a26f9be94a
sujoy_capgemini <sujoy.ghoshal@capgemini.com>
2025-07-08 12:40:23 +0530

    [sujoy]adding File Handaling|Preprocessor|Macros|Dsa -problem

---
+++ File Handling/Macros.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define PI 3.14159265359
+int main() {
+    //Code
+    printf("%.100f",PI);
+    return 0;
+}
---
+++ File Handling/Preprocessor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    printf("Hello\n");
+    float x=sqrt(2);
+    int max=__INT_MAX__;
+    printf("%d\n",x);
+    printf("%d\n",max);
+    return 0;
+}
